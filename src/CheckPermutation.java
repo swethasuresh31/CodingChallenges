3158b2e234ecef86d1b76b4c2bb36c4d32916e37
swethasuresh31 <swetha.suresh@sjsu.edu>
2019-08-27 13:01:17 -0700

    find if unique characters in string

---
+++ src/CheckPermutation.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.util.HashMap;
+
+public class CheckPermutation {
+    public boolean ifInputsPermutationOfEachOther(String s1, String s2){
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        HashMap<Character,Integer> checkMap = new HashMap<>();
+
+        //traversing through the first string and storing it in hash map as <"character",no of occurence>
+       for(int i=0; i<s1.length();i++){
+           int value = 1;
+           if(checkMap.containsKey(s1.charAt(i))){
+               value = checkMap.get(s1.charAt(i)) + 1;
+               checkMap.put(s1.charAt(i), value);
+           }
+           else{
+               checkMap.put(s1.charAt(i), value);
+           }
+       }
+
+       //traversing through the second string and matching character occurences
+        for(int i = 0;i<s2.length();i++){
+            if(checkMap.containsKey(s2.charAt(i))){
+                int value = checkMap.get(s2.charAt(i));
+                if(value == 1){
+                    checkMap.remove(s2.charAt(i));
+                }else{
+                    checkMap.put(s2.charAt(i), value-1);
+                }
+            }
+        }
+
+        if(!checkMap.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        CheckPermutation obj1 = new CheckPermutation();
+        System.out.println(obj1.ifInputsPermutationOfEachOther("abcd","dbca"));
+        System.out.println(obj1.ifInputsPermutationOfEachOther("abcda","dbca"));
+        System.out.println(obj1.ifInputsPermutationOfEachOther("abc0","0bca"));
+        System.out.println(obj1.ifInputsPermutationOfEachOther("abcde","fbcae"));
+    }
+
+}
--- src/FindIfStringIsUnique.java
+++ src/FindIfStringIsUnique.java
@@ -3,6 +3,9 @@ import java.util.HashSet;
 public class FindIfStringIsUnique {
 
     public boolean hasUniqueCharacters(String s){
+        if(s == null){
+            throw new IllegalArgumentException();
+        }
         HashSet<Character> uniqueSet = new HashSet<>();
         for(int i=0; i<s.length();i++){
             if(uniqueSet.contains(s.charAt(i))){
@@ -14,11 +17,47 @@ public class FindIfStringIsUnique {
         return true;
     }
 
+    public boolean bruteForceHasUniqueCharacters(String s) {
+        if(s == null){
+            throw new IllegalArgumentException();
+        }
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<s.length();j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args){
+
+        //checking with data structures
         FindIfStringIsUnique obj = new FindIfStringIsUnique();
         System.out.println(obj.hasUniqueCharacters("abcd"));
         System.out.println(obj.hasUniqueCharacters("abbd"));
         System.out.println(obj.hasUniqueCharacters("0abc"));
+        System.out.println(obj.hasUniqueCharacters(""));
+
+        //checking without data structures
+        System.out.println(obj.bruteForceHasUniqueCharacters("abcd"));
+        System.out.println(obj.bruteForceHasUniqueCharacters("abbd"));
+        System.out.println(obj.bruteForceHasUniqueCharacters("0abc"));
+
+        try {
+            System.out.println(obj.bruteForceHasUniqueCharacters(null));
+        }catch(Exception e){
+            System.out.println("illegal argument: "+e);
+
+        }
+
+        try {
+            System.out.println(obj.hasUniqueCharacters(null));
+        }catch(Exception e){
+            System.out.println("illegal argument: "+e);
+
+        }
     }
 
 }
